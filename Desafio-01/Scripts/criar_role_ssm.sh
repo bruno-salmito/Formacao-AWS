34538be1e0ed740928b04b4f1570b5d9ef059f49
Bruno Salmito <brunosalmito@gmail.com>
2025-08-14 20:02:22 -0300

    Add Desafio-02

--- Desafio-01/Scripts/criar_role_ssm.sh
+++ Desafio-01/Scripts/criar_role_ssm.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+#----------------------------------------------
+#Script: criar_role_ssm.sh
+#Descrição: Cria uma IAM role para acesso ao SSM, esse script deve ser executado no cloud shell da AWS.
+#Data: versão 1.0
+#Uso: ./criar_role_ssm.sh
+#----------------------------------------------
+
+
 role_name="role-acesso-ssm"
 policy_name="AmazonSSMManagedInstanceCore"
 
+# Verifica se a role já existe
 if aws iam get-role --role-name "$role_name" &> /dev/null; then
     echo "A IAM role $role_name já existe."
     exit 1
